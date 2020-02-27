6e19ffc493dafedd140308a56729b13c4be52eea
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-27 14:27:13 +0530

    ADDED:Set value for win or loss as Re 1

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -3,6 +3,22 @@
 #Constants
 BET=1
 #Variables
-stake=100 
-
+stake=100
+win=0
+lose=0
+randomCheck=0
+counter=1
 echo "Welcome to gambling simulation"
+#Generate two randim values
+randomCheck=$((RANDOM%2))
+case $randomCheck in
+	0) echo "You lost"
+		((lose++))
+		stake=$((stake-BET))
+	;;
+	1) echo "You won"
+		((win++))
+		stake=$((stake+BET))
+	;;
+esac
+echo "updated stake:$stake"

b0008102b822a032e19b3d789cf940a64d491e39
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-27 14:37:26 +0530

    MODIFIED: Corrected indentations

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -9,14 +9,14 @@ lose=0
 randomCheck=0
 counter=1
 echo "Welcome to gambling simulation"
-#Generate two randim values
+#Generate two random values
 randomCheck=$((RANDOM%2))
 case $randomCheck in
-	0) echo "You lost"
+	0)	echo "You lost"
 		((lose++))
 		stake=$((stake-BET))
 	;;
-	1) echo "You won"
+	1)	echo "You won"
 		((win++))
 		stake=$((stake+BET))
 	;;
